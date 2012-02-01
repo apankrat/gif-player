1a8ae7efa0dbd47f973380471e40678c6329994d
Alex Pankratov <ap@swapped.cc>
2012-01-31 17:46:02 -0800

    replaced 'busy' with 'done' callback

--- gif-player.js
+++ gif-player.js
@@ -13,31 +13,45 @@
 
 function gifPlayer(cont, opts) {
 
-	/* parameters */
+	/* 
+	 *	parameters
+	 */
 	var defaults = {
 		autoplay: true,
-		play:     function(s,m,b) { s.hide(); m.show() },
-		stop:     function(m,s,b) { m.hide(); s.show() }
+		play:     function(s, m, cb) { s.hide(); m.show(); cb(); },
+		stop:     function(m, s, cb) { m.hide(); s.show(); cb(); }
 	};
 
 	var opts = $.extend({}, defaults, opts);
 	
-	/* variables */
+	/*
+	 *	variables
+	 */
 	var c = cont;
 	var s = cont.find('.gif-still');
 	var m = cont.find('.gif-movie');
-	var i = new Image;
 
 	var state = 'e';
 	var busy = false;
 
-	/* functions */
+	var i = new Image;
+
+	/* 
+	 *	functions
+	 */
 	var setState = function(was, now) {
 		c.removeClass('gif-player-' + was);
 		c.addClass('gif-player-' + now);
 		state = now;
 	}
 
+	var play = opts['play'];
+	var stop = opts['stop'];
+	var done = function() { busy = false; }
+
+	/* 
+	 *	click() handler
+	 */
 	this.act = function()
 	{
 		if (busy)
@@ -54,13 +68,15 @@ function gifPlayer(cont, opts) {
 				i.src = m.attr('src');
 				m.unbind('load');
 
-				if (opts['autoplay'])
+				if (! opts['autoplay'])
 				{
-					setState('l','p');
-					opts['play'](s, m, busy);
-				}
-				else
 					setState('l','s');
+					return;
+				}
+
+				setState('l','p');
+				busy = true;
+				play(s, m, done);
 			});
 			m.attr('src', m.attr('gif'));
 			break;
@@ -76,19 +92,24 @@ function gifPlayer(cont, opts) {
 
 			/* this rewinds the gif, not in all browsers */
 			m.attr('src', null).attr('src', i.src);
-			
+
 			setState('s', 'p');
-			opts['play'](s, m, busy);
+			busy = true;
+			play(s, m, done);
 			break;
 		
 		case 'p': /* playing... */
 
 			setState('p', 's');
-			opts['stop'](m, s, busy);
+			busy = true;
+			stop(m, s, done);
 			break;
 		}
 	}
 	
+	/*
+	 *	initialization
+	 */
 	var that = this;
 
 	c.find('.gif-ctrl').click( function(){ that.act(); });
